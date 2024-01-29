65590627b9c2e3c9361c2add24748be4ba435eef
Jeff Ripke <jripke74@gmail.com>
2024-01-29 09:22:51 -0800

    add tabContent

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,20 @@ function App() {
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
-    console.log(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -39,7 +52,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {tabContent}
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,4 +1,3 @@
 export default function TabButton({ children, onSelect }) {
-  console.log('TAB BUTTON EXECUTING');
   return <button onClick={onSelect}>{children}</button>;
 }
